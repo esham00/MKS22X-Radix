b9afefbc66feba867c64dc7065025ac88d3e8bb2
Elizabeth Sham <esham00@stuy.edu>
2019-03-31 20:58:18 -0400

    added remove first

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,15 +1,19 @@
+import java.util.*;
 public class MyLinkedList<E>{
     private int size;
-    private Node start, end;
-    public MyLinkedList(Node first, Node last) {
+    private Node<E> start, end;
+    @SuppressWarnings("unchecked")     
+    public MyLinkedList(Node<E> first, Node<E> last) {
 	start = first;
 	end = last;
 	size = 2;
     }
+    @SuppressWarnings("unchecked")
     public MyLinkedList() {
 	size = 0;
     }
-    public MyLinkedList(Node value) {
+    @SuppressWarnings("unchecked")     
+    public MyLinkedList(Node<E> value) {
 	start = value;
     }
     public int size() {
@@ -18,16 +22,16 @@ public class MyLinkedList<E>{
     public boolean add(E value) {
 	//adding to the start
 	if (size == 0) {
-	    start = new Node(value);
+	    start = new Node<E>(value);
 	}
 	//initializing end if there is only one value
 	else if (size == 1) {
-	    end = new Node(value, start, null);
+	    end = new Node<E>(value, start, null);
 	    start.setNext(end);
 	}
 	else {
 	    //creating a new end
-	    Node temp = new Node(value, end, null);
+	    Node<E> temp = new Node<E>(value, end, null);
 	    //creating a link between the old end and new end
 	    end.setNext(temp);
 	    //setting the end to the new end
@@ -38,7 +42,7 @@ public class MyLinkedList<E>{
     }
     public String toString() {
 	String output = "[";
-	Node current = start;
+	Node<E> current = start;
 	int i = 0;
 	while (current != null) {
 	    if (current.getNext() == null) {
@@ -51,38 +55,13 @@ public class MyLinkedList<E>{
 	}
 	return output + "]";
     }
+    @SuppressWarnings("unchecked")     
     private Node getNthNode(int index) {
-	Node current = start; 
+	Node<E> current = start; 
 	for(int i = 0; i < index; i++) {
 	    current = current.getNext();
 	}
 	return current;
-    }
-    public void add (int index, E value) {
-	//exception : index out of bounds
-	if (index < 0 || index > size) {
-	    throw new IndexOutOfBoundsException(index + " is out of bounds");
-	}
-	//adding at start
-	if (index == 0) {
-	    Node temp = new Node(value, null, start);
-	    start.setPrev(temp);
-	    start = temp;
-	    size++;
-	}
-	//adding at end
-	else if (index == size) {
-	    add(index);
-	}
-	//if not then there must be an index before and after
-	else {
-	    Node after = getNthNode(index);
-	    Node before = after.getPrev();
-	    Node wedge = new Node(value, before, after);
-	    after.setPrev(wedge);
-	    before.setNext(wedge);
-	    size++;
-	}
     }
     public void clear() {
 	size = 0;
@@ -108,25 +87,42 @@ public class MyLinkedList<E>{
 	    other.clear();
 	}
      }
+    public E removeFront() {
+	if (size == 0) {
+	    throw new NullPointerException();
+	} else if (size == 1) {
+	    E old = start.getData();
+	    start = null;
+	    end = null;
+	    size--;
+	    return old;
+	} else {
+	    E old = start.getData();
+	    start = start.getNext();
+	    start.setPrev(null);
+	    size--;
+	    return old;
+	}
+    }
 }
-
-class Node {
-    private Integer data;
-    private Node next, prev;
-    public Node(Integer input, Node before, Node after) {
+@SuppressWarnings("unchecked")     
+class Node<E> {
+    private E data;
+    private Node<E> next, prev;
+    public Node(E input, Node<E> before, Node<E> after) {
     	data = input;
     	prev = before;
     	next = after;
     }
-    public Node(Integer input) {
+    public Node(E input) {
 	data = input;
     }
-    public Node(Node input) {
+    public Node(Node<E> input) {
 	data = input.getData();
 	next = input.getNext();
 	prev = input.getPrev();
     }
-    public Integer getData() {
+    public E getData() {
 	return data;
     }
     public Node getNext() {
@@ -135,15 +131,15 @@ class Node {
     public Node getPrev() {
 	return prev;
     }
-    public Integer setData(Integer value) {
-	int temp = data;
+    public E setData(E value) {
+	E temp = data;
 	data = value;
         return temp;
     }
-    public void setNext(Node after) {
+    public void setNext(Node<E> after) {
 	next = after;
     }
-    public void setPrev(Node before) {
+    public void setPrev(Node<E> before) {
 	prev = before;
     }
     public String toString() {
