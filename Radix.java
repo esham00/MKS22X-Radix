716d7f12f268c91a66cf82ae6c219a61a7845aa6
Elizabeth Sham <esham00@stuy.edu>
2019-04-04 00:30:59 -0400

    almost there

--- Radix.java
+++ Radix.java
@@ -23,13 +23,15 @@ public class Radix {
 	    }
 	}
 	combine(merge, buckets);
+	System.out.println(merge);
 	for(int i = 0; i < largestDigit;i++) {
-	    for(int a = 0; a < data.length; a++) {
+	    for(int a = 0; a < merge.size(); a++) {
 		int removed = merge.removeFront();
-		int digit = getValue(removed, (int)Math.pow(10, i));
+		int digit = getValue(removed, (int)Math.pow(10, i+1));
 		if (removed < 0) {
 		    buckets[9-digit].add(removed);
 		} else {
+		    System.out.println(removed);
 		    buckets[digit+10].add(removed);
 		}
 	    }
@@ -40,27 +42,37 @@ public class Radix {
 	}
     }
     private static int getValue(int value, int mod){
-	return value%mod;
+	System.out.println(mod);
+        if (mod != 10 && value % mod == value) {
+	     return 0;
+	} else {
+	    return Math.abs(value%mod);
+	}
     }
     private static int length(int value) {
 	int num = Math.abs(value);
 	return (int)(Math.log10(num)+1);
     }
     private static void combine(MyLinkedList<Integer> a, MyLinkedList<Integer>[] b) {
-	// int tracker = 0;
-	// try {
-	    for(int i = 0; i < b.length; i++) {
+	//try {
+	for(int i = 0; i < b.length; i++) {
+	    if (b[i].size() > 0) {
 		a.extend(b[i]);
-	// 	System.out.println(a);
-	// 	tracker++;
 	    }
+	    //System.out.println("merge in combine" + a);
+	}
 	// } catch (NullPointerException e) {
-	//     System.out.println(b[tracker]);
-	//     System.out.println(a);
+	//     System.out.println("element failure:" + b[tracker]);
+	//     //System.out.println(a);
 	//     System.out.println(tracker);
-	//}
+	// }
     }
     public static void main(String[] args) {
+	// MyLinkedList x = new MyLinkedList();
+	// x.add(1);
+	// MyLinkedList b = new MyLinkedList();
+	// b.extend(x);
+	// System.out.println(b);
 	int[] a = new int[]{10,9,3,4,5,2,1,6,7,8};
 	radixsort(a);
 	System.out.print(Arrays.toString(a));
